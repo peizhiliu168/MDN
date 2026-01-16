b274af2837ab9795cf944b009689cad6c2ca7f76
Peizhi Liu <peizhiliu168@gmail.com>
2026-01-16 17:24:16 -0600

    added slurm scripts

--- CommPatterns/slurm/file_mine_qpe.sh
+++ CommPatterns/slurm/file_mine_qpe.sh
@@ -2,17 +2,17 @@
 #SBATCH -A p31538
 #SBATCH -t 48:00:00
 #SBATCH -p normal
-#SBATCH --job-name="file_mine_adder"
+#SBATCH --job-name="file_mine_qpe"
 #SBATCH --mail-type=BEGIN,END,NONE,FAIL,REQUEUE
 #SBATCH --mail-user=peizhiliu2023@u.northwestern.edu
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=48
 #SBATCH --mem=192G
-#SBATCH --output=/home/plh2448/projects/MDN/CommPatterns/slurm/file_mine_adder.out
-#SBATCH --error=/home/plh2448/projects/MDN/CommPatterns/slurm/file_mine_adder.err
+#SBATCH --output=/home/plh2448/projects/MDN/CommPatterns/slurm/file_mine_qpe.out
+#SBATCH --error=/home/plh2448/projects/MDN/CommPatterns/slurm/file_mine_qpe.err
 
 source /home/plh2448/projects/MDN/.venv/bin/activate
 
 cd /home/plh2448/projects/MDN/CommPatterns
 
-./run_parallel_file_mining.sh adder 2 20 100000
+./run_parallel_file_mining.sh qpe 2 20 100000
